15fb0ccc48c592dff402ea2308158fb398d2d9b2
vincetita <vince.tita@gmail.com>
2020-03-11 21:32:00 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container,Dimmer, Loader } from "semantic-ui-react";
+import Home from './components/Home'
+import People from './components/People'
+import Planets from './components/Planets'
+import Films from './components/Films'
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [films, setFilms] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchPeople() {
+      let res = await fetch("https://swapi.co/api/people/?format=json");
+      let data = await res.json();
+      setPeople(data.results);
+      setLoading(false)
+    }
+
+    async function fetchPlanets() {
+      let res = await fetch("https://swapi.co/api/planets/?format=json");
+      let data = await res.json();
+      setPlanets(data.results);
+      setLoading(false)
+      console.log(data.results)
+
+    }async function fetchFilms() {
+      let res = await fetch("https://swapi.co/api/films/?format=json");
+      let data = await res.json();
+      setFilms(data.results);
+      setLoading(false)
+      console.log(data.results)
+    }
+
+
+    fetchFilms()
+    fetchPeople();
+    fetchPlanets();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Container>
+          {loading ? (
+            <Dimmer active inverted>
+              <Loader inverted>Loading</Loader>
+            </Dimmer>
+          ):(
+            <Switch>
+            <Route exact path='/'>
+            <Home/>
+            </Route>
+            <Route exact path='/people'>
+              <People data={people}/>
+            </Route>
+            <Route exact path='/planets'>
+              <Planets data= {planets}/>
+            </Route>
+            <Route exact path='/films'>
+              <Films data= {films}/>
+            </Route>
+          </Switch>
+          )}
+          
+        </Container>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Films.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Grid, Card } from 'semantic-ui-react'
+
+export default function Films({data}) {
+    return (
+        <>
+        <h1>Planets</h1>
+        <Grid columns={5}>
+            {data.map((planet, i)=>{
+                return(
+                    <Grid.Column key={i}>
+                        <Card>
+                            <Card.Content>
+                                <Card.Header>{planet.name}</Card.Header>
+                                <Card.Description>
+                                    <strong>Climate</strong>
+                                    <p>{planet.climate}</p>
+                                    <strong>Diameter</strong>
+                                    <p>{planet.diameter}</p>
+                                    <strong>Population</strong>
+                                    <p>{planet.population}</p>
+                                </Card.Description>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                )
+            })}
+        </Grid>
+    </>
+    )
+}
+
